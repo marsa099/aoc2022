c855046adb43682c9e52606c1bcedc259691875d
Martin Sandberg <martin@koderiet.dev>
2022-12-01 08:31:12 +0100

    Task 1 completed

---
+++ 1.cs
@@ -0,0 +1,34 @@
+
+
+public static class Task1
+{
+    public static void Execute()
+    {
+        var input = File.ReadAllLines("inputs/input-1.txt");
+
+
+        var allBackpackTotal = new List<int>();
+
+        int currentBackpack = 0;
+
+        foreach (var line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                allBackpackTotal.Add(currentBackpack);
+                currentBackpack = 0;
+                continue;
+            }
+            currentBackpack += int.Parse(line);
+        }
+
+        // 1a
+        Console.WriteLine(allBackpackTotal.Max());
+
+
+        // 1b
+        allBackpackTotal.Sort();
+        allBackpackTotal.Reverse();
+        Console.WriteLine(string.Join(",", allBackpackTotal.Take(3).Sum()));
+    }
+}
