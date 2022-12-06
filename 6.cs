f72a5eb4fb50aa4322491e2b43e065465e8a8fb1
Martin Sandberg <martin@koderiet.dev>
2022-12-06 08:18:16 +0100

    6b finished

--- 6.cs
+++ 6.cs
@@ -4,7 +4,6 @@ public static class Task6
     public static void Execute()
     {
         var input = File.ReadAllText("inputs/input-6.txt");
-        int total = 0;
 
         int skipCount = 0;
         int takeCount = 4;
@@ -22,6 +21,22 @@ public static class Task6
         }
 
         Console.WriteLine($"6a: {skipCount + takeCount}");
+
+        skipCount = 0;
+        takeCount = 14;
+        complete = false;
+        while (!complete)
+        {
+            var next = input.Skip(skipCount).Take(takeCount);
+            if (next.Distinct().Count() == next.Count())
+            {
+                complete = true;
+                break;
+            }
+            skipCount++;
+        }
+
+        Console.WriteLine($"6b: {skipCount + takeCount}");
     }
 
 }
