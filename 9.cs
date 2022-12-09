2b61f03aa282c197b013e840639a5495d9d88cc7
Martin Sandberg <martin@koderiet.dev>
2022-12-09 15:23:57 +0100

    Nope. didnt solve it now either. Need to calculate the way until each destination for each step

--- 9.cs
+++ 9.cs
@@ -2,6 +2,8 @@ using System.Drawing;
 
 public static class Task9
 {
+
+    // TODO: Take into account the positions the tail travels. Not only the final position for each move......
     public static void Execute()
     {
         var input = File.ReadAllLines("inputs/input-9.txt");
@@ -11,23 +13,26 @@ public static class Task9
         Point head = new Point(0, 0);
         Point tail = new Point(0, 0);
 
+        int steps = 0;
+
         foreach (var line in input)
         {
             previousHeadPosition = head;
 
+            steps = int.Parse(line.Split(' ')[1].ToString());
             switch (line.Split(' ')[0])
             {
                 case "U":
-                    --head.Y;
+                    head.Y -= steps;
                     break;
                 case "R":
-                    ++head.X;
+                    head.X += steps;
                     break;
                 case "L":
-                    --head.X;
+                    head.X -= steps;;
                     break;
                 case "D":
-                    ++head.Y;
+                    head.Y += steps;
                     break;
                 default:
                     Console.WriteLine("This should not happen");
@@ -46,12 +51,48 @@ public static class Task9
 
     private static bool NeedToMoveTheTail(Point head, Point tail)
     {
-        var xDiff = Math.Abs(head.X - tail.X);
-        var yDiff = Math.Abs(head.Y - tail.Y);
+        int xDiff = 0;
+        int yDiff = 0;
 
-        // TODO: This is probably not rights
-        if (xDiff > 1 || yDiff > 1 || xDiff + yDiff > 2)
+        Console.Write($"head: ({head.X},{head.Y}) tail: ({tail.X},{tail.Y}) ");
+
+        // Case 1: Both points positive
+        if (head.X >= 0 && tail.X >= 0)
+            xDiff = Math.Abs(head.X - tail.X);
+        // Case 2: Both points negative
+        else if (head.X < 0 && tail.X < 0)
+            xDiff = Math.Abs(Math.Abs(head.X) - Math.Abs(tail.X));
+        // Case 3: Head positive, tail negative
+        else if (head.X >= 0 && tail.X < 0) // 7--3 = 10
+            xDiff = Math.Abs(head.X - tail.X);
+        // Case 4  Head negative, tail positive
+        else if (head.X < 0 && tail.X >= 0)
+            xDiff = Math.Abs(tail.X - head.X);
+        else
+            Console.WriteLine($"Scenario not covered: head.X: {head.X} tail.X: {tail.X}");
+
+
+        // Case 1: Both points positive
+        if (head.Y >= 0 && tail.Y >= 0)
+            yDiff = Math.Abs(head.Y - tail.Y);
+        // Case 2: Both points negative
+        else if (head.Y < 0 && tail.Y < 0)
+            yDiff = Math.Abs(Math.Abs(head.Y) - Math.Abs(tail.Y));
+        // Case 3: Head positive, tail negative
+        else if (head.Y >= 0 && tail.Y < 0) // 7--3 = 10
+            yDiff = Math.Abs(head.Y - tail.Y);
+        // Case 4  Head negative, tail positive
+        else if (head.Y < 0 && tail.Y >= 0)
+            yDiff = Math.Abs(tail.Y - head.Y);
+        else
+            Console.WriteLine($"Scenario not covered: head.Y: {head.Y} tail.Y: {tail.Y}");
+
+        if (xDiff > 1 || yDiff > 1)
+        {
+            Console.WriteLine(" MOVE");
             return true;
+        }
+        Console.WriteLine("");
         return false;
     }
 }
