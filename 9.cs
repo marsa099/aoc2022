deb058fc0b2ad53630849500e468482979f31c4c
Martin Sandberg <martin@koderiet.dev>
2022-12-11 00:22:37 +0100

    Something maybe

--- 9.cs
+++ 9.cs
@@ -6,19 +6,17 @@ public static class Task9
     // TODO: Take into account the positions the tail travels. Not only the final position for each move......
     public static void Execute()
     {
-        var input = File.ReadAllLines("inputs/input-9.txt");
+        var input = File.ReadAllLines("inputs/input-9 copy.txt");
 
-        List<Point> visitedPoints = new List<Point>();
         Point previousHeadPosition = new Point(0, 0);
         Point head = new Point(0, 0);
         Point tail = new Point(0, 0);
-        List<Point> tailMoves;
+        List<Point> visitedPoints = new List<Point>{tail};
 
         int steps = 0;
 
         foreach (var line in input)
         {
-            tailMoves = new List<Point>();
             steps = int.Parse(line.Split(' ')[1].ToString());
             previousHeadPosition = head;
             Console.Write($"Instruction: {line}: ");
@@ -26,44 +24,57 @@ public static class Task9
             switch (line.Split(' ')[0])
             {
                 case "U":
-                    // 
-                    for (int i = head.Y -1; i > head.Y - steps; i--)
-                        tailMoves.Add(new Point(head.X, i));
-                    head.Y -= steps;
-                    break;
-                case "R":
-                    for (int i = head.X +1; i < head.X + steps; i++)
-                        tailMoves.Add(new Point(i, head.Y));
-                    head.X += steps;
-                    break;
-                case "L":
-                    for (int i = head.X -1; i > head.X - steps; i--)
-                        tailMoves.Add(new Point(i, head.Y));
-                    head.X -= steps; ;
+                    for (int i = head.Y; i >= previousHeadPosition.Y - steps; i--)
+                    {
+                        Console.WriteLine("For loop #1");
+                        head.Y = i;
+                        if (NeedToMoveTheTail(head, tail))
+                        {
+                            tail = new Point(head.X, head.Y + 1);
+                            visitedPoints.Add(tail);
+                        }
+                    }
                     break;
                 case "D":
-                    for (int i = head.Y +1; i < head.Y + steps; i++)
-                        tailMoves.Add(new Point(head.X, i));
-                    head.Y += steps;
+                    for (int i = head.Y; i <= previousHeadPosition.Y + steps; i++)
+                    {
+                        Console.WriteLine("For loop #2");
+                        head.Y = i;
+                        if (NeedToMoveTheTail(head, tail))
+                        {
+                            tail = new Point(head.X, head.Y - 1);
+                            visitedPoints.Add(tail);;
+                        }
+                    }
+                    break;
+                case "R":
+                    for (int i = head.X; i <= previousHeadPosition.X + steps; i++)
+                    {
+                        Console.WriteLine("For loop #3");
+                        head.X = i;
+                        if (NeedToMoveTheTail(head, tail))
+                        {
+                            tail = new Point(head.X - 1, head.Y);
+                            visitedPoints.Add(tail);
+                        }
+                    }
+                    break;
+                case "L":
+                    for (int i = head.X; i >= previousHeadPosition.X - steps; i--)
+                    {
+                        Console.WriteLine("For loop #4");
+                        head.X = i;
+                        if (NeedToMoveTheTail(head, tail))
+                        {
+                            tail = new Point(head.X + 1, head.Y);
+                            visitedPoints.Add(tail);
+                        }
+                    }
+                        
                     break;
                 default:
                     Console.WriteLine("This should not happen");
                     break;
-            }
-            // Move the tail if needed
-            if (NeedToMoveTheTail(head, tail))
-            {
-                if(tailMoves.Any())
-                {
-                    visitedPoints.AddRange(tailMoves);
-                    tail = tailMoves.Last();
-                }
-                else
-                {
-                    visitedPoints.Add(previousHeadPosition);
-                    tail = previousHeadPosition;
-                }
-                Console.WriteLine($" {tailMoves.Count()}");
             }
         }
         Console.WriteLine("Visisted points: " + visitedPoints.Count());
@@ -72,15 +83,18 @@ public static class Task9
 
     private static bool NeedToMoveTheTail(Point head, Point tail)
     {
-        int xDiff = 0;
-        int yDiff = 0;
-
         Console.Write($"head: ({head.X},{head.Y}) tail: ({tail.X},{tail.Y}) ");
 
-        xDiff = head.X - tail.X;
-        yDiff = head.Y - tail.Y;
+        double xDiff = head.X - tail.X;
+        double yDiff = head.Y - tail.Y;
 
         Console.WriteLine("");
-        return Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2)) > 1;
+
+        if (Math.Abs(xDiff) > 1 || Math.Abs(yDiff) > 1)
+            return true;
+
+        return false;
+
+        //return Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2)) >= 2;
     }
 }
