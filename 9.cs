c7166102abe99bbb56fb490d5a3b69978ae2a18e
Martin Sandberg <martin@koderiet.dev>
2022-12-10 23:20:21 +0100

    Allt är skit

--- 9.cs
+++ 9.cs
@@ -6,7 +6,7 @@ public static class Task9
     // TODO: Take into account the positions the tail travels. Not only the final position for each move......
     public static void Execute()
     {
-        var input = File.ReadAllLines("inputs/input-9 copy.txt");
+        var input = File.ReadAllLines("inputs/input-9.txt");
 
         List<Point> visitedPoints = new List<Point>();
         Point previousHeadPosition = new Point(0, 0);
@@ -20,26 +20,29 @@ public static class Task9
         {
             tailMoves = new List<Point>();
             steps = int.Parse(line.Split(' ')[1].ToString());
+            previousHeadPosition = head;
+            Console.Write($"Instruction: {line}: ");
+
             switch (line.Split(' ')[0])
             {
                 case "U":
                     // 
-                    for (int i = head.Y; i > head.Y - steps; i--)
+                    for (int i = head.Y -1; i > head.Y - steps; i--)
                         tailMoves.Add(new Point(head.X, i));
                     head.Y -= steps;
                     break;
                 case "R":
-                    for (int i = head.X; i < head.X + steps; i++)
+                    for (int i = head.X +1; i < head.X + steps; i++)
                         tailMoves.Add(new Point(i, head.Y));
                     head.X += steps;
                     break;
                 case "L":
-                    for (int i = head.X; i > head.X - steps; i--)
+                    for (int i = head.X -1; i > head.X - steps; i--)
                         tailMoves.Add(new Point(i, head.Y));
                     head.X -= steps; ;
                     break;
                 case "D":
-                    for (int i = head.Y; i < head.Y + steps; i++)
+                    for (int i = head.Y +1; i < head.Y + steps; i++)
                         tailMoves.Add(new Point(head.X, i));
                     head.Y += steps;
                     break;
@@ -50,8 +53,17 @@ public static class Task9
             // Move the tail if needed
             if (NeedToMoveTheTail(head, tail))
             {
-                tail = tailMoves.Last();
-                visitedPoints.AddRange(tailMoves);
+                if(tailMoves.Any())
+                {
+                    visitedPoints.AddRange(tailMoves);
+                    tail = tailMoves.Last();
+                }
+                else
+                {
+                    visitedPoints.Add(previousHeadPosition);
+                    tail = previousHeadPosition;
+                }
+                Console.WriteLine($" {tailMoves.Count()}");
             }
         }
         Console.WriteLine("Visisted points: " + visitedPoints.Count());
@@ -65,43 +77,10 @@ public static class Task9
 
         Console.Write($"head: ({head.X},{head.Y}) tail: ({tail.X},{tail.Y}) ");
 
-        // Case 1: Both points positive
-        if (head.X >= 0 && tail.X >= 0)
-            xDiff = Math.Abs(head.X - tail.X);
-        // Case 2: Both points negative
-        else if (head.X < 0 && tail.X < 0)
-            xDiff = Math.Abs(Math.Abs(head.X) - Math.Abs(tail.X));
-        // Case 3: Head positive, tail negative
-        else if (head.X >= 0 && tail.X < 0) // 7--3 = 10
-            xDiff = Math.Abs(head.X - tail.X);
-        // Case 4  Head negative, tail positive
-        else if (head.X < 0 && tail.X >= 0)
-            xDiff = Math.Abs(tail.X - head.X);
-        else
-            Console.WriteLine($"Scenario not covered: head.X: {head.X} tail.X: {tail.X}");
+        xDiff = head.X - tail.X;
+        yDiff = head.Y - tail.Y;
 
-
-        // Case 1: Both points positive
-        if (head.Y >= 0 && tail.Y >= 0)
-            yDiff = Math.Abs(head.Y - tail.Y);
-        // Case 2: Both points negative
-        else if (head.Y < 0 && tail.Y < 0)
-            yDiff = Math.Abs(Math.Abs(head.Y) - Math.Abs(tail.Y));
-        // Case 3: Head positive, tail negative
-        else if (head.Y >= 0 && tail.Y < 0) // 7--3 = 10
-            yDiff = Math.Abs(head.Y - tail.Y);
-        // Case 4  Head negative, tail positive
-        else if (head.Y < 0 && tail.Y >= 0)
-            yDiff = Math.Abs(tail.Y - head.Y);
-        else
-            Console.WriteLine($"Scenario not covered: head.Y: {head.Y} tail.Y: {tail.Y}");
-
-        if (xDiff > 1 || yDiff > 1)
-        {
-            Console.WriteLine(" MOVE");
-            return true;
-        }
         Console.WriteLine("");
-        return false;
+        return Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2)) > 1;
     }
 }
