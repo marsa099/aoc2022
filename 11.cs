043505419a736fae6c7b62d4ece568e99f2e20ca
Martin Sandberg <martin@koderiet.dev>
2022-12-11 19:44:29 +0100

    Checkpoint

--- 11.cs
+++ 11.cs
@@ -1,10 +1,11 @@
 public class Monkey
 {
     public int Id;
-    public List<int> Items = new List<int>();
+    public Queue<int> Items = new Queue<int>();
     public Func<int, int> Operation;
     public int TestDivision;
     public bool Test(int x) => x % TestDivision == 0;
+    public int ThrowTo(int x) => Test(x) ? SendToIfTrue : SendToIfFalse;
     public int SendToIfTrue;
     public int SendToIfFalse;
 }
@@ -15,7 +16,7 @@ public static class Task11
 
     public static void Execute()
     {
-        var input = File.ReadAllLines("inputs/input-11.txt");
+        var input = File.ReadAllLines("inputs/input-11-testdata.txt");
 
         var x = new Monkey { Id = 1, Operation = (oldValue) => { return oldValue * 19; } };
 
@@ -24,21 +25,41 @@ public static class Task11
         foreach (var metadata in input.Chunk(7))
         {
             var monkeyId = int.Parse(metadata[0].Replace("Monkey ", "").Replace(":", ""));
-            var items = metadata[1].Replace("  Starting items: ", "").Split(", ").Select(x => int.Parse(x)).ToList(); // Kolla om vi behöver göra reverse här?
+            var items = metadata[1].Replace("  Starting items: ", "").Split(", ").Select(x => int.Parse(x)); // Kolla om vi behöver göra reverse här?
             var operation = ParseOperation(metadata[2].Replace("  Operation: new = ", ""));
             var testDivision = int.Parse(metadata[3].Replace("  Test: divisible by ", ""));
             var sendToIfTrue = int.Parse(metadata[4].Replace("    If true: throw to monkey ", ""));
             var sendToIfFalse = int.Parse(metadata[5].Replace("    If false: throw to monkey ", ""));
 
-            monkeys.Add(new Monkey
+            var parsedMonkey = new Monkey
             {
                 Id = monkeyId,
-                Items = items,
                 Operation = operation,
+                Items = new Queue<int>(items),
                 TestDivision = testDivision,
                 SendToIfTrue = sendToIfTrue,
                 SendToIfFalse = sendToIfFalse
-            });
+            };
+            monkeys.Add(parsedMonkey);
+
+            foreach(var monkey in monkeys)
+            {
+                if (!monkey.Items.TryDequeue(out int itemToThrow))
+                    continue;
+
+                // Increase worryLevel on item
+                Console.WriteLine($"Before inspect: {itemToThrow}");
+                itemToThrow = monkey.Operation(itemToThrow);
+                Console.WriteLine($"Inspect: {itemToThrow}");
+                // No worries, decrease worry level
+                itemToThrow /= 3;
+                Console.WriteLine($"No worries: {itemToThrow}");
+
+                monkey.Test(itemToThrow);
+
+                Console.WriteLine($"Will throw to {monkey.ThrowTo(itemToThrow)}");
+
+            }
 
             //Console.WriteLine($"'{monkeyId}' '{string.Join(", ", items)}' operation on 3 {operation(3)}");
         }
